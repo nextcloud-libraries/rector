7bbed686f883e9247991a89d8279e5e5f5a62c48
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-09-10 20:26:00 +0200

    test: restore dummy tests
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ tests/NullTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Christophwurst\Test\Nextcloud\Rector;
+
+class NullTest extends TestCase
+{
+    public function testGreet(): void
+    {
+        self::assertTrue(true);
+    }
+}
