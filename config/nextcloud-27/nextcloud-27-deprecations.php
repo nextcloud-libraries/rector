9bd7c3d199000bf0fc9bbc2012bcbaa4d64cd974
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-09-11 10:09:51 +0200

    fix: do not import classes by default
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- config/nextcloud-27/nextcloud-27-deprecations.php
+++ config/nextcloud-27/nextcloud-27-deprecations.php
@@ -7,7 +7,6 @@ use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->importNames();
     $rectorConfig->ruleWithConfiguration(
         AnnotationToAttributeRector::class,
         [
