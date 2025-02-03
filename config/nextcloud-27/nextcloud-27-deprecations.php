63da32666f307f32764ababf9da42846aee8a16b
Côme Chilliet <come.chilliet@nextcloud.com>
2025-02-03 12:34:30 +0100

    feat: Add rules for all annotations replaced by attributes in 27
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- config/nextcloud-27/nextcloud-27-deprecations.php
+++ config/nextcloud-27/nextcloud-27-deprecations.php
@@ -12,9 +12,24 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(
         AnnotationToAttributeRector::class,
         [
+            new AnnotationToAttribute('AnonRateLimit', 'OCP\AppFramework\Http\Attribute\AnonRateLimit'),
+            new AnnotationToAttribute('ARateLimit', 'OCP\AppFramework\Http\Attribute\ARateLimit'),
+            new AnnotationToAttribute(
+                'AuthorizedAdminSetting',
+                'OCP\AppFramework\Http\Attribute\AuthorizedAdminSetting',
+            ),
+            new AnnotationToAttribute('BruteForceProtection', 'OCP\AppFramework\Http\Attribute\BruteForceProtection'),
+            new AnnotationToAttribute('CORS', 'OCP\AppFramework\Http\Attribute\CORS'),
             new AnnotationToAttribute('NoAdminRequired', 'OCP\AppFramework\Http\Attribute\NoAdminRequired'),
-            new AnnotationToAttribute('PublicPage', 'OCP\AppFramework\Http\Attribute\PublicPage'),
             new AnnotationToAttribute('NoCSRFRequired', 'OCP\AppFramework\Http\Attribute\NoCSRFRequired'),
+            new AnnotationToAttribute(
+                'PasswordConfirmationRequired',
+                'OCP\AppFramework\Http\Attribute\PasswordConfirmationRequired',
+            ),
+            new AnnotationToAttribute('PublicPage', 'OCP\AppFramework\Http\Attribute\PublicPage'),
+            new AnnotationToAttribute('StrictCookiesRequired', 'OCP\AppFramework\Http\Attribute\StrictCookiesRequired'),
+            new AnnotationToAttribute('SubAdminRequired', 'OCP\AppFramework\Http\Attribute\SubAdminRequired'),
+            new AnnotationToAttribute('UserRateLimit', 'OCP\AppFramework\Http\Attribute\UserRateLimit'),
         ],
     );
 };
