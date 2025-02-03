32c1a22e35f3c34ff825e402eeb5b4557f31e7e3
Côme Chilliet <come.chilliet@nextcloud.com>
2025-02-03 12:11:33 +0100

    fix: Add empty sets for versions 28-30
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- src/Set/NextcloudSets.php
+++ src/Set/NextcloudSets.php
@@ -14,4 +14,7 @@ final class NextcloudSets implements SetListInterface
     public const NEXTCLOUD_25 = __DIR__ . '/../../config/nextcloud-25/nextcloud-25-deprecations.php';
     public const NEXTCLOUD_26 = __DIR__ . '/../../config/nextcloud-26/nextcloud-26-deprecations.php';
     public const NEXTCLOUD_27 = __DIR__ . '/../../config/nextcloud-27/nextcloud-27-deprecations.php';
+    public const NEXTCLOUD_28 = self::NEXTCLOUD_27;
+    public const NEXTCLOUD_29 = self::NEXTCLOUD_27;
+    public const NEXTCLOUD_30 = self::NEXTCLOUD_27;
 }
